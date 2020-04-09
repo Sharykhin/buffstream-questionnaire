a6ccdc307f0cf6ba6763dd9e9c88907b7855b144
Sharykhin <siarhei.sharykhin@gmail.com>
2020-04-09 21:15:51 +0300

    added test on parsing query params as int64

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/lib/pq v1.3.0
 	github.com/mattn/go-nulltype v0.0.0-20200221160555-75ae8a76f2e9
 	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d
+	github.com/stretchr/testify v1.5.1
 )
---
+++ http/controller/common_test.go
@@ -0,0 +1,51 @@
+package controller
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestQueryParamAsInt64(t *testing.T) {
+	tt := []struct {
+		name          string
+		inKey         string
+		inValue       string
+		expectedKey   string
+		expectedValue int64
+		defaultValue  int64
+	}{
+		{
+			name:          "Set limit",
+			inKey:         "limit",
+			inValue:       "20",
+			expectedKey:   "limit",
+			expectedValue: 20,
+			defaultValue:  10,
+		},
+		{
+			name:          "Return default value",
+			inKey:         "",
+			inValue:       "",
+			expectedKey:   "limit",
+			expectedValue: 10,
+			defaultValue:  10,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			url := fmt.Sprintf("?%s=%s", tc.inKey, tc.inValue)
+			req, err := http.NewRequest("GET", url, nil)
+			if err != nil {
+				t.Errorf("failed to create test http request: %v", err)
+			}
+			v, err := QueryParamAsInt64(req, tc.expectedKey, tc.defaultValue)
+			assert.Equal(t, tc.expectedValue, v)
+			assert.Nil(t, err)
+
+		})
+	}
+}
