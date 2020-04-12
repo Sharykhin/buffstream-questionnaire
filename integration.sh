14fb8cb50f8eb589081f911e2bd91bdb05ae46e4
Sharykhin <siarhei.sharykhin@gmail.com>
2020-04-12 16:17:25 +0300

    fixed integration file

--- integration.sh
+++ integration.sh
@@ -2,10 +2,7 @@
 
 docker-compose -f docker-compose.integration.yml up -d postgres
 sleep 5
-#docker-compose -f docker-compose.integration.yml up -d rest
-docker-compose -f docker-compose.integration.yml up -d sql-migration
-sleep 5
-
+docker-compose -f docker-compose.integration.yml up sql-migration
 for file in `find ./database/fixtures | grep -i '.sql'`; do
     echo "importing fixture $file";
     docker exec -i buff_postgres_test psql -U postgres buff < "$file";
